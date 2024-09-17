5455707204d9050b9896c7eaea833ee5a750d24a
markusok <sagalaev.m@gmail.com>
2024-09-17 13:07:39 +0400

    add eureka-client and eureka-server

---
+++ eureka-client/src/main/java/ru/micro/demo/eurekaclient/controller/DemoController.java
@@ -0,0 +1,13 @@
+package ru.micro.demo.eurekaclient.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/demo")
+    public String demo() {
+        return "demo";
+    }
+}
